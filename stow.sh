c67ed293ba8e55ce9a680448997efb07d84408c6
lander247 <electronicmail837@gmail.com>
2024-12-26 00:40:18 -0700

    moved some things from bootstrapper into here

--- stow.sh
+++ stow.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-stow --verbose --restow --target="$HOME" .
+local_folder="$HOME/.local"
+bin_folder="$HOME/.local/bin"
+
+mkdir "$local_folder"
+mkdir "$bin_folder"
+
+stow --verbose --dotfiles --restow --target="$HOME" .
