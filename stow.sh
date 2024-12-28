3f5a7ea0a211c5ac4d33abfb7e65047dbcfc7096
lander247 <electronicmail837@gmail.com>
2024-12-27 18:51:47 -0700

    small change

--- stow.sh
+++ stow.sh
@@ -2,10 +2,10 @@
 
 # Make sure folder locations used in stowing exist to prevent undesired sym-linking
 local_folder="$HOME/.local"
-bin_folder="$HOME/.local/bin"
+local_bin_folder="$HOME/.local/bin"
 share_folder="$HOME/.local/share"
 mkdir "$local_folder"
-mkdir "$bin_folder"
+mkdir "$local_bin_folder"
 mkdir "$share_folder"
 
 stow --verbose --dotfiles --restow --target="$HOME" .
