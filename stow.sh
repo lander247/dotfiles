febed4ee72bf33ab4e50dcf3801884b9684c5f84
lander247 <electronicmail837@gmail.com>
2024-12-30 10:39:12 -0700

    added back xresources because im stupid

--- stow.sh
+++ stow.sh
@@ -3,9 +3,9 @@
 # Make sure folder locations used in stowing exist to prevent undesired sym-linking
 local_folder="$HOME/.local"
 local_bin_folder="$HOME/.local/bin"
-share_folder="$HOME/.local/share"
+local_share_folder="$HOME/.local/share"
 mkdir "$local_folder"
 mkdir "$local_bin_folder"
-mkdir "$share_folder"
+mkdir "$local_share_folder"
 
 stow --verbose --dotfiles --restow --target="$HOME" .
